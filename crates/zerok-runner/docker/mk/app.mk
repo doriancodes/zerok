2b9a1e46587811c0a18b2eb1d49d6231735caa36
doriancodes <dorian.alo.davi@gmail.com>
2025-08-14 23:41:48 +0200

    basic sel4 setup

---
+++ crates/zerok-runner/docker/mk/app.mk
@@ -0,0 +1,62 @@
+#
+# Copyright 2024, Colias Group, LLC
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+root_dir := $(dir $(lastword $(MAKEFILE_LIST)))/..
+
+build_dir := build
+
+.PHONY: none
+none:
+
+.PHONY: clean
+clean:
+	rm -rf $(build_dir)
+
+$(build_dir):
+	mkdir -p $(build_dir)
+
+sel4_prefix := $(SEL4_INSTALL_DIR)
+
+loader_artifacts_dir := $(SEL4_INSTALL_DIR)/bin
+loader := $(loader_artifacts_dir)/sel4-kernel-loader
+loader_cli := $(loader_artifacts_dir)/sel4-kernel-loader-add-payload
+
+app := $(build_dir)/app.elf
+
+image := $(build_dir)/image.elf
+
+$(image): $(app) $(loader) $(loader_cli)
+	$(loader_cli) \
+		--loader $(loader) \
+		--sel4-prefix $(sel4_prefix) \
+		--app $(app) \
+		-o $@
+
+qemu_cmd = \
+	qemu-system-aarch64 \
+		-machine virt,virtualization=on -cpu cortex-a53 -m size=2G \
+		-serial mon:stdio \
+		-nographic \
+		-kernel $(image) \
+		$(extra_qemu_args)
+
+.PHONY: simulation-context
+simulation-context: $(image)
+
+.PHONY: simulate
+simulate: simulation-context
+	$(qemu_cmd)
+
+.PHONY: test
+test: test.py simulation-context
+	PYTHONPATH=$(root_dir)/test-utils python3 $< $(qemu_cmd)
+
+common_cargo_args := \
+	--config 'env.SEL4_PREFIX="$(sel4_prefix)"' \
+	--target-dir $(build_dir)/target \
+	--artifact-dir $(build_dir)
+
+FORCE:
--- crates/zerok-runner/src/main.rs
+++ crates/zerok-runner/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("zerok-runner: hello from the workspace!");
+#![no_std]
+#![no_main]
+
+use sel4_root_task::root_task;
+
+#[root_task]
+fn main(_bootinfo: &sel4::BootInfoPtr) -> ! {
+    sel4::debug_println!("Hello, World!");
+
+    sel4::init_thread::suspend_self()
 }
